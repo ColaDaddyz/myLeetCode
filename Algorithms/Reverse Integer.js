f181cd4939ebf5a9a721c3b5c803e788c65116b0
zhangzhe.zhang <zhangzhe.zhang@qunar.com>
2017-02-07 20:04:28 +0800

    Reverse

---
+++ Algorithms/Reverse Integer.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var min = -2147483648;
+var max = 2147483647;
+var reverse = function (x) {
+    var minuZero = x < 0;
+    var intStr = "" + (minuZero ? -x : x);
+    var reverseStr = intStr.split('').reverse().join('');
+    var reverseInt = parseInt(reverseStr);
+    if(reverseInt)
+        if(minuZero){
+            reverseInt = -reverseInt
+        }
+    if(reverseInt<min || reverseInt>max){
+        return 0
+    }
+    return reverseInt
+};
+console.log(reverse(0));
