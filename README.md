f1fc3cfbdc359a6522b1182cc7fa560edf23e7ee
ZhangFe <zhangzhe0214@gmail.com>
2017-02-14 22:02:06 +0800

    修改md，修改文件命名方式

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # myLeetCode
 LeetCode solutions with JavaScript
 
+我自己刷Leetcode的一些答案，欢迎大家交流
+
 已完成
 Two Sum
 Reverse Integer
+
+#TODO 
+自动抓取leetcode获取已完成哪些，还差哪些
--- test/Two-Sum.test.js
+++ test/Two-Sum.test.js
@@ -1,3 +1,13 @@
 /**
  * Created by zhangzhe on 2017/2/4.
  */
+var should = require('should');
+var twoSum = require('./Algorithms/Two-Sum.js');
+
+
+describe('Two-Sum', function () {
+    it("1", function () {
+        1
+    })
+});
+
